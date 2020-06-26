804c0aba82eb60929a36c9a2b5eb08cfd659b45f
Dierk Koenig <dierk.koenig@canoo.com>
2020-06-26 02:39:50 +0200

    avoid default package, print repl start commands for copying into console

--- runFrepl.sh
+++ runFrepl.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "starting frege repl for file $1"
-echo ":l $1" | java -jar lib/org/frege-lang/frege/3.25.84/frege-3.25.84.jar
+echo "copy the following to a plain terminal:"
+echo ""
+echo "./gradlew --console=plain --no-daemon fregeRepl"
+echo ":l $1"
+echo ":browse $2"
+
